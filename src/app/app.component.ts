5e13a0dd25f49a3b062b1fdbcfc677025a43bad8
Shivam Patel <shivampatel251097@gmail.com>
2020-10-26 17:16:21 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,11 +12,46 @@ export class AppComponent {
   answer = '';
   genders = ['Male', 'Female'];
   defaultGender = 'Female';
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
   // onSubmit(form: NgForm){
   //   console.log(form);
   //   console.log(form.value);
   // }
+  suggestedUserName() {
+    const suggestedName = 'ShivamPatel251097';
+    //setvalue used for  changing all values
+    // this.signUpForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: 'shibbu251097@gmail.com'
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: 'Rabbit',
+    //   gender: 'Male'
+    // });
+    //patch value to overwrite only 1 value.
+    this.signUpForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
+  }
+
   onSubmit(){
     console.log(this.signUpForm);
+    this.submitted = true;
+    this.user.username = this.signUpForm.value.userData.username;
+    this.user.email = this.signUpForm.value.userData.email;
+    this.user.gender = this.signUpForm.value.gender;
+    this.user.answer = this.signUpForm.value.questionAnswer;
+    this.user.secretQuestion = this.signUpForm.value.secret;
+    this.signUpForm.reset();
   }
 }
