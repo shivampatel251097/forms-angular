0ab048283b8762db5c36f6c49c1ba00f75d8f533
Shivam Patel <shivampatel251097@gmail.com>
2020-10-26 16:18:22 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'handling-forms-angular';
+  @ViewChild('f') signUpForm: NgForm;
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['Male', 'Female'];
+  defaultGender = 'Female';
+  // onSubmit(form: NgForm){
+  //   console.log(form);
+  //   console.log(form.value);
+  // }
+  onSubmit(){
+    console.log(this.signUpForm);
+  }
 }
